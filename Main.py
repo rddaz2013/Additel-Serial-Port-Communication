90d6e81ffb9d2b75f5ac5c77a63cf6cb687f5eb8
Michael Pope <michael.pope.email@gmail.com>
2017-06-01 12:12:41 -0700

    Start2

--- Main.py
+++ Main.py
@@ -2,4 +2,4 @@ import serial.tools.list_ports
 ports = list(serial.tools.list_ports.comports())
 for p in ports:
     print p
-var = raw_input("Pres Enter to continue: ")
+var = raw_input("Press Enter to continue: ")
