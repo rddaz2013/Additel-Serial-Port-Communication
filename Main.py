84baba82a2e1588bb41373ff5180a37fc11b7fdf
Michael Pope <michael.pope.email@gmail.com>
2017-06-01 16:04:39 -0700

    Edited our gitignore to ignore temporary files

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import serial.tools.list_ports
 ports = list(serial.tools.list_ports.comports())
-for p in ports:
-    print p
-var = raw_input("Press Enter to continue: ")
+units = []
+for port in ports:
+    print port.device
+    open_port = serial.Serial(port.device, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 1)
+    open_port.close()
